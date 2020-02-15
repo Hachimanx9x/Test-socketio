5325ad5a1fa36585a7de1410a6503a96ad4e97df
Hachimanx9x <bolanosd38@gmail.com>
2020-02-14 22:29:58 -0500

    se puso la nueva conexion

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const app = require('express')();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
+const PORT = 80;
 app.get('/', (req, res) =>{
 //  res.send("<h1>hola mundo desde udemy</h1>")
-res.sendFile(__dirname + "/index.html"); 
+res.sendFile(__dirname + "/index.html");
 });
-http.listen(80, ()=>{
-  console.log(`inciado desde el puerto ${80}`)
-})
+
+io.on('connection', (socket) =>{
+  console.log('un usuario conectado');
+});
+
+http.listen(PORT, ()=>{
+  console.log(`iniciado en el puerto : ${PORT}`);
+});
