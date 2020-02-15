8505dd8f5205b3f80846f15ca6d4233577b2b41e
Hachimanx9x <bolanosd38@gmail.com>
2020-02-15 12:12:26 -0500

    se agrego la funcion de reconocer conexion desde el html

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
+//su usa express para iniciar el servidor http
 const app = require('express')();
+//se usa los protocolos http junto con el servidor de express
 const http = require('http').Server(app);
+//se le dice a socket.io que use el servidor http para comprobar las conexiones de lso hilos de xonexion
 const io = require('socket.io')(http);
-
+//Se inicializa el puerto
 const PORT = 80;
+//se habilita la primera entrada por defecto
 app.get('/', (req, res) =>{
-//  res.send("<h1>hola mundo desde udemy</h1>")
+//se le dice que por defecto cargue index.html
 res.sendFile(__dirname + "/index.html");
 });
-
+//al iniciar una conexion en hilo de envio de datos
 io.on('connection', (socket) =>{
-  console.log('un usuario conectado');
+  //cuando se ve una nueva coneccion al cargar el html
+  //<<ir a linea de index.html 18>>
+  //el html manda al server diciendo es una nueva conexion
+  console.log(`se conecto usuario`);
+  //Cuando se pierde la conexion se ejecuta esta funcion
+  socket.on('disconnect',() =>{
+    //cuando ocure las desconexion se imprime
+    console.log(`de desconectado`);
+  });
 });
 
 http.listen(PORT, ()=>{
+  //iniciamos el servidor en el puerot establecido 
   console.log(`iniciado en el puerto : ${PORT}`);
 });
