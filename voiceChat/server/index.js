6e627d481b14f21eff52bb5b636bd4c847c76f3f
Hachimanx9x <diego_fer.bolanos@uao.edu.co>
2021-03-18 21:17:16 -0500

    se mejoro el metodo para el renocimiento de los usuarios que entran

--- voiceChat/server/index.js
+++ voiceChat/server/index.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const http = require('http').Server(app);
 const cors = require('cors');
 const io = require('socket.io')(http, {
-  cors:  {
+  cors: {
     origin: "*",
     methods: ["GET", "POST"]
   }
@@ -10,40 +10,77 @@ const io = require('socket.io')(http, {
 
 const PORT = 3030;
 app.use(cors());
-app.get('/', (req, res) =>{
-//se le dice que por defecto cargue index.html
-res.json({msj:"hola"})
+app.get('/', (req, res) => {
+  //se le dice que por defecto cargue index.html
+  res.json({ msj: "hola" })
 });
 
 //se inicio la coneccion
-io.on('connection', (socket) =>{
+io.on('connection', (socket) => {
   //se notifica que usuario se conecto
   console.log(`user with id ${socket.id} is connected`)
   //evento de ingresar a una sala
-  socket.on('entra room',(room)=>{
+  socket.on('entra room', (room) => {
     //se notivica que sala es creada / ingresada
-  console.log(`Una nuevo usuario entro a la sala ${room}`);
+    console.log(`Una nuevo usuario entro a la sala ${room}`);
     //se le asigna el objeto de la sala (datos del usuario)
-  socket.room =room;
+    socket.room = room;
     //se define el hilo de conexion con esa sala
-  socket.join(room);
-  //se emite un mensaje a todos los que esten en esa sala del nuevo usuario
-  io.in(socket.room).emit('entrar',{iserid:socket.id, room  });
+    socket.join(room);
+    // console.log(getuserroom(room))
+
+    //se emite un mensaje a todos los que esten en esa sala del nuevo usuario
+    io.in(socket.room).emit('entrar', { iserid: socket.id, room: getuserroom(room, socket.id) });
 
   });
-  socket.on('disconnect',() =>{
+  socket.on('disconnect', () => {
     //cuando ocure las desconexion se imprime
     console.log(`se desconecto el usuario ${socket.id}`);
+    io.in(socket.room).emit('chao', { iserid: socket.id });
   });
   //se escucha el evento hablar
-  socket.on('hablar',(data)=>{
+  socket.on('hablar', (data) => {
     // se emite un objeto con lo que dice esa persona y su id
-io.in(socket.room).emit('hablar',{raw: data, id :socket.id });
+    io.in(socket.room).emit('hablar', { raw: data, id: socket.id });
   });
 })
 
-http.listen(PORT, ()=>{
+http.listen(PORT, () => {
 
   //iniciamos el servidor en el puerot establecido
   console.log(`iniciado en el puerto : ${PORT}`);
 });
+
+function getuserroom(room, id) {
+  const nsp = io.of('/');
+  const rooms = nsp.adapter.rooms.get(room);
+  let list = []
+  for (const clientId of rooms) {
+    const clientSocket = io.sockets.sockets.get(clientId);
+    list.push(clientSocket.id)
+  }
+
+
+  return list
+}
+
+/*
+//el que mantine mostrando a los usuarios
+setInterval(function () {
+  // io.sockets.adapter.rooms.get('Room Name');
+  const nsp = io.of('/');
+  const clients = nsp.adapter.rooms.get('sala1');
+  const numClients = clients ? clients.size : 0;
+  let pack = [];
+  let listRooms = []
+  console.log(clients)
+  for (const clientId of clients) {
+    //este es el socket de cada cliente en la room.
+    const clientSocket = io.sockets.sockets.get(clientId);
+    consolelog(clientSocket)
+  }
+  console.log(clients)
+  //console.log(rooms)
+
+
+}, 1500)*/
